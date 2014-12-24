d37b8b5b20822dbaae115154b8b360470a511777
rememberlenny <rememberlenny@gmail.com>
2014-12-24 18:14:18 -0500

    Save the update before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_filter :ensure_signup_complete, only: [:new, :create, :update, :destroy]
   def ensure_signup_complete
     # Ensure we don't go into an infinite loop
     return if action_name == 'finish_signup'
