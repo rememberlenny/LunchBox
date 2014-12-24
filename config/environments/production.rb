fb530fea430655e0f8e305bb1e1006dade478a2f
rememberlenny <rememberlenny@gmail.com>
2014-12-24 18:45:03 -0500

    Deliver

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
-    # enable_starttls_auto: true,
+    enable_starttls_auto: true,
     user_name: 'ENV["LB_GMAIL_UR"]',
     password: 'ENV["LB_GMAIL_PW"]',
     authentication: :plain,
