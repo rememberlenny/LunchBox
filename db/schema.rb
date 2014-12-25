0f2ddb403928e1542467127660aaf3e2523397e1
rememberlenny <rememberlenny@gmail.com>
2014-12-25 18:02:21 -0500

    Save the controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225225127) do
+ActiveRecord::Schema.define(version: 20141225225215) do
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -62,6 +62,16 @@ ActiveRecord::Schema.define(version: 20141225225127) do
     t.integer  "invitations_count",      default: 0
     t.string   "uid"
     t.string   "avatar"
+    t.boolean  "tod_morning_early"
+    t.boolean  "tod_morning_middle"
+    t.boolean  "tod_morning_late"
+    t.boolean  "dow_monday"
+    t.boolean  "dow_tuesday"
+    t.boolean  "dow_wednesday"
+    t.boolean  "dow_thursday"
+    t.boolean  "dow_friday"
+    t.boolean  "dow_saturday"
+    t.boolean  "dow_sunday"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
