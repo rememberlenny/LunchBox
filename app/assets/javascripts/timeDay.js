e2c9efe85527c68922d543a99c500c5373b04609
rememberlenny <rememberlenny@gmail.com>
2014-12-25 01:33:42 -0500

    Save the order of the forms

--- app/assets/javascripts/timeDay.js
+++ app/assets/javascripts/timeDay.js
@@ -1,2 +1,11 @@
-document.querySelector('.time-of-day').value
-document.querySelector('.day-of-week').value
+// var tod = document.querySelector('.time-of-day').value;
+// var dow = document.querySelectorAll('.day-of-week');
+// var checked = [];
+
+// for(i = 0; i < dow.length; i++){
+//   if(dow[i].checked){
+//     checked.push(i);
+//   }
+// }
+
+// var checked = checked.join('-');
