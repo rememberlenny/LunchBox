89a7c93f37ce4ea4489a28ec976f0f7b30806cd3
rememberlenny <rememberlenny@gmail.com>
2014-12-24 23:08:11 -0500

    Save the users route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :meetings
 
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
-
+  get 'users' => 'static_pages#home'
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
