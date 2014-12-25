a3b9eeb5ee995f77da6d2c6698fc1e49562bf8bf
rememberlenny <rememberlenny@gmail.com>
2014-12-25 01:47:40 -0500

    Change the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
+  get 'schedule' => 'user#assign_meeting'
+
   root 'static_pages#home'
 
   # Example of regular route:
