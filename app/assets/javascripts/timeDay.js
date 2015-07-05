155e30143e0d592c578b038653169179d78beb1b
rememberlenny <rememberlenny@gmail.com>
2015-07-04 21:37:58 -0400

    Display what is being stored

--- app/assets/javascripts/timeDay.js
+++ app/assets/javascripts/timeDay.js
@@ -9,8 +9,8 @@
 // }
 
 // var checked = checked.join('-');
-
-var savedData = {};
+var LB = LB || {};
+LB.savedData = {};
 
 $('.toggle li').on('click', function(e){
   var $this = $(this);
@@ -20,20 +20,47 @@ $('.toggle li').on('click', function(e){
     $this.addClass('selected');
   }
   checkSelected();
+  updateReport();
+  console.log(LB.savedData);
 });
 
+function updateReport(){
+  var options = ['daysOfWeek', 'neighborhood'];
+  var reports = {};
+  var $reportContent = $('.report-content');
+  $reportContent.each(function(i, item){
+    $(item).html('');
+  });
+  for(i = 0; i < options.length; i++){
+    var reportName = options[i]+'-report';
+    if(reports[reportName] == undefined){
+      reports[reportName] = [];
+    }
+    console.log(options[i]);
+    option = LB.savedData[options[i]];
+    console.log(option);
+    if(option != undefined){
+      for(j = 0; j < option.length; j++){
+        reports[reportName].push(option[j]);
+      }
+      var reportContent = reports[reportName].join(', ');
+      console.log('reportContent: ' + reportContent);
+      $('.'+reportName).html(reportContent);
+    }
+  }
+}
+
 function checkSelected(){
+  LB.savedData = {};
   var cat,
-      savedData = {},
       $selected = $('.toggle li.selected');
   $selected.each(function(i, item){
     var $item = $(item);
     cat = $item.attr('title');
-    if(savedData[cat] == undefined){
-      savedData[cat] = [];
+    if(LB.savedData[cat] == undefined){
+      LB.savedData[cat] = [];
     }
-    savedData[cat].push($item.attr('name'));
+    LB.savedData[cat].push($item.attr('name'));
   });
-  console.log(savedData);
 }
 
