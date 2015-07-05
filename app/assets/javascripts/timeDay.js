5643cab83e1263f7ac428cfb328002a7a2550e70
rememberlenny <rememberlenny@gmail.com>
2015-07-04 21:17:11 -0400

    Save the data structure

--- app/assets/javascripts/timeDay.js
+++ app/assets/javascripts/timeDay.js
@@ -9,3 +9,31 @@
 // }
 
 // var checked = checked.join('-');
+
+var savedData = {};
+
+$('.toggle li').on('click', function(e){
+  var $this = $(this);
+  if( $this.hasClass('selected') ){
+    $this.removeClass('selected');
+  } else {
+    $this.addClass('selected');
+  }
+  checkSelected();
+});
+
+function checkSelected(){
+  var cat,
+      savedData = {},
+      $selected = $('.toggle li.selected');
+  $selected.each(function(i, item){
+    var $item = $(item);
+    cat = $item.attr('title');
+    if(savedData[cat] == undefined){
+      savedData[cat] = [];
+    }
+    savedData[cat].push($item.attr('name'));
+  });
+  console.log(savedData);
+}
+
