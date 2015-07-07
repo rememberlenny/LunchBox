8cffa0f38bb8079b6527eea241055a49ed75a511
rememberlenny <rememberlenny@gmail.com>
2015-07-06 21:28:52 -0400

    Set up cancel

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:setup, :show, :assign_meeting, :edit, :update, :destroy]
+  before_action :set_user, only: [:account_cancel, :setup, :show, :assign_meeting, :edit, :update, :destroy]
 
   # GET /users/:id.:format
   def show
