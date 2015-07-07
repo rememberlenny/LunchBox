2cfd31a8ed80fad0dca1cd3d800465d7a3905cea
rememberlenny <rememberlenny@gmail.com>
2015-07-07 11:32:24 -0400

    Token

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,8 +1,8 @@
 TWITTER = Twitter::REST::Client.new do |config|
-  config.consumer_key        = LB_TWITTER_CONSUMER_KEY
-  config.consumer_secret     = LB_TWITTER_CONSUMER_SECRET
-  config.access_token        = LB_TWITTER_ACCESS_TOKEN
-  config.access_token_secret = LB_TWITTER_ACCESS_SECRET
+  config.consumer_key        = ENV['LB_TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['LB_TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['LB_TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['LB_TWITTER_ACCESS_SECRET']
 end
 
 
