361bb5c636798ef7f6c94e9068f2a86dc19c5c9d
rememberlenny <rememberlenny@gmail.com>
2015-07-07 09:12:20 -0400

    Get the home page reflecting the qualities needed on the home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  def current_user_status_active
+    if !current_user.nil?
+
+    end
+  end
+
+  def current_user_locations_active
+    if !current_user.nil?
+      places = ['loc_bushwick', 'loc_downtown', 'loc_harlem', 'loc_midtown', 'loc_queens', 'loc_redhook', 'loc_ues', 'loc_uws', 'loc_williamsburg']
+      places.each do |place|
+        if current_user[place] == true
+          return true
+        end
+      end
+      return false
+    else
+      return false
+    end
+  end
+
+  def current_user_days_active
+    if !current_user.nil?
+      times = ['dow_mo', 'dow_tu', 'dow_we', 'dow_th', 'dow_fr', 'dow_sa', 'dow_su']
+      times.each do |time|
+        if current_user[time] == true
+          return true
+        end
+      end
+      return false
+    else
+      return false
+    end
+  end
 end
